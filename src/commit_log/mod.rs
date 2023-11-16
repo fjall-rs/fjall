21fe6b54f45850c2d3d1d3be91fc7dea99240477
marvin-j97 <marvin.janke.97@gmail.com>
2023-11-16 18:46:27 +0100

    test: commit log write

--- src/commit_log/mod.rs
+++ src/commit_log/mod.rs
@@ -10,69 +10,114 @@ use std::{
     fs::{File, OpenOptions},
     io::{BufWriter, Write},
     path::Path,
+    sync::{Mutex, MutexGuard},
 };
 
 /// The commit logs durably stores items before they are added to the [`MemTable`]
 /// to allow recovering the in-memory state after a failure
 pub struct CommitLog {
-    writer: BufWriter<File>,
+    writer: Mutex<BufWriter<File>>,
+}
+
+struct Writer;
+
+impl Writer {
+    /// Writes a batch start marker to the commit log
+    fn write_start(
+        writer: &mut MutexGuard<BufWriter<File>>,
+        len: u32,
+    ) -> Result<(), SerializeError> {
+        Marker::Start(len).serialize(writer.get_mut())
+    }
+
+    /// Writes a batch end marker to the commit log
+    fn write_end(writer: &mut MutexGuard<BufWriter<File>>, crc: u32) -> Result<(), SerializeError> {
+        Marker::End(crc).serialize(writer.get_mut())
+    }
+
+    /// Writes a batch item to the commit log
+    fn write_item(
+        writer: &mut MutexGuard<BufWriter<File>>,
+        item: Value,
+    ) -> Result<Vec<u8>, SerializeError> {
+        let mut bytes = Vec::new();
+        Marker::Item(item).serialize(&mut bytes)?;
+        writer.write_all(&bytes)?;
+        Ok(bytes)
+    }
 }
 
 impl CommitLog {
     /// Flushes the commit log file
-    pub(crate) fn flush(&mut self) -> std::io::Result<()> {
-        self.writer.flush()?;
-        self.writer.get_mut().sync_all()
+    pub(crate) fn flush(&self) -> std::io::Result<()> {
+        let mut writer = self.writer.lock().expect("should lock mutex");
+
+        writer.flush()?;
+        writer.get_mut().sync_all()
     }
 
     /// Creates a new commit log file to append data to
     pub(crate) fn new<P: AsRef<Path>>(path: P) -> std::io::Result<Self> {
         let file = OpenOptions::new().append(true).create(true).open(path)?;
-        let writer = BufWriter::with_capacity(128_000, file);
+        let writer = Mutex::new(BufWriter::with_capacity(128_000, file));
 
         Ok(Self { writer })
     }
 
-    /// Appends a single entry wrapped in a batch to the commit log
-    pub(crate) fn append(&mut self, entry: Value) -> Result<(), SerializeError> {
-        self.append_batch(vec![entry])
-    }
-
-    /// Writes a batch start marker to the commit log
-    fn write_start(&mut self, len: u32) -> Result<(), SerializeError> {
-        Marker::Start(len).serialize(&mut self.writer)
-    }
-
-    /// Writes a batch end marker to the commit log
-    fn write_end(&mut self, crc: u32) -> Result<(), SerializeError> {
-        Marker::End(crc).serialize(&mut self.writer)
-    }
-
-    /// Writes a batch item to the commit log
-    fn write_item(&mut self, item: Value) -> Result<Vec<u8>, SerializeError> {
-        let mut bytes = Vec::new();
-        Marker::Item(item).serialize(&mut bytes)?;
-        self.writer.write_all(&bytes)?;
-        Ok(bytes)
+    /// Appends a single item wrapped in a batch to the commit log
+    pub(crate) fn append(&self, item: Value) -> Result<(), SerializeError> {
+        self.append_batch(vec![item])
     }
 
     /// Appends a batch to the commit log
-    pub(crate) fn append_batch(&mut self, items: Vec<Value>) -> Result<(), SerializeError> {
+    pub(crate) fn append_batch(&self, items: Vec<Value>) -> Result<(), SerializeError> {
         let mut hasher = crc32fast::Hasher::new();
-        //let mut bytes = 0;
+
+        let mut writer = self.writer.lock().expect("should lock mutex");
 
         // NOTE: entries.len() is surely never > u32::MAX
         #[allow(clippy::cast_possible_truncation)]
-        self.write_start(items.len() as u32)?;
+        Writer::write_start(&mut writer, items.len() as u32)?;
 
-        for entry in items {
-            let serialized = self.write_item(entry)?;
-
-            // bytes += serialized.len();
+        for item in items {
+            let serialized = Writer::write_item(&mut writer, item)?;
             hasher.update(&serialized);
         }
 
-        self.write_end(hasher.finalize())?;
+        Writer::write_end(&mut writer, hasher.finalize())?;
+
+        Ok(())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use tempfile::tempdir;
+
+    #[test]
+    fn test_write() -> std::io::Result<()> {
+        let folder = tempdir()?.into_path();
+        let log_path = folder.join("commit_log");
+        let commit_log = CommitLog::new(&log_path)?;
+
+        let items = vec![
+            Value::new(vec![1, 2, 3], vec![4, 5, 6], false, 42),
+            Value::new(vec![7, 8, 9], vec![], false, 43),
+        ];
+
+        commit_log.append_batch(items).unwrap();
+        commit_log.flush()?;
+
+        let file_content = std::fs::read(log_path)?;
+
+        assert_eq!(
+            &[
+                0, 0, 0, 0, 2, 2, 131, 84, 123, 163, 1, 0, 0, 0, 0, 0, 0, 0, 42, 0, 0, 3, 1, 2, 3,
+                0, 0, 0, 3, 4, 5, 6, 1, 0, 0, 0, 0, 0, 0, 0, 43, 0, 0, 3, 7, 8, 9, 0, 0, 0, 0
+            ],
+            &*file_content
+        );
 
         Ok(())
     }
