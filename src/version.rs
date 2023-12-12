d8d1a2ea9e0512223fe22a8e4f4fac02b1691d54
marvin-j97 <marvin.janke.97@gmail.com>
2023-12-12 19:17:57 +0100

    version = "0"

--- src/version.rs
+++ src/version.rs
@@ -8,7 +8,7 @@ pub enum Version {
 
 impl std::fmt::Display for Version {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "1")
+        write!(f, "0")
     }
 }
 
