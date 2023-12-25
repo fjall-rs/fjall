ea8044aa2b5c81d25772c62acee6edd0790eba1f
marvin-j97 <marvin.janke.97@gmail.com>
2023-12-25 20:52:40 +0100

    refactor

--- lsm-tree/src/memtable/mod.rs
+++ lsm-tree/src/memtable/mod.rs
@@ -47,10 +47,6 @@ impl MemTable {
                 return None;
             }
 
-            if &*key.user_key != prefix {
-                continue;
-            }
-
             // Check for seqno if needed
             if let Some(seqno) = seqno {
                 if key.seqno < seqno {
