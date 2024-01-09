362b10f39893beafbe4d07fea90c279d8b2fea96
marvin-j97 <marvin.janke.97@gmail.com>
2024-01-09 04:00:36 +0100

    refactor & test bloom filter

--- lsm-tree/src/bloom.rs
+++ lsm-tree/src/bloom.rs
@@ -3,7 +3,6 @@ use crate::{DeserializeError, SerializeError};
 use bit_vec::BitVec;
 use byteorder::{BigEndian, ReadBytesExt, WriteBytesExt};
 use seahash::SeaHasher;
-use std::f32::consts::LN_2;
 use std::fs::File;
 use std::hash::Hasher;
 use std::io::{BufReader, BufWriter, Read, Write};
@@ -21,12 +20,6 @@ pub struct BloomFilter {
 
     /// Number of hash functions
     k: usize,
-}
-
-fn calculate_m(item_count: usize, false_positive_rate: f32) -> usize {
-    let ln2_squared = LN_2.powi(2);
-    let result = -((item_count as f32) * false_positive_rate.ln()) / ln2_squared;
-    ((result / 8.0).ceil() * 8.0) as usize
 }
 
 impl Serializable for BloomFilter {
@@ -74,6 +67,16 @@ impl BloomFilter {
         }
     }
 
+    pub(crate) fn calculate_m(n: usize, fp_rate: f32) -> usize {
+        use std::f32::consts::LN_2;
+
+        let n = n as f32;
+        let ln2_squared = LN_2.powi(2);
+
+        let m = -(n * fp_rate.ln() / ln2_squared);
+        ((m / 8.0).ceil() * 8.0) as usize
+    }
+
     /// Constructs a bloom filter that can hold `item_count` items
     /// while maintaining a certain false positive rate.
     pub fn with_fp_rate(item_count: usize, fp_rate: f32) -> Self {
@@ -81,7 +84,7 @@ impl BloomFilter {
         let fp_rate = fp_rate.max(0.000_001);
 
         let k = 7;
-        let m = calculate_m(item_count, fp_rate);
+        let m = Self::calculate_m(item_count, fp_rate);
 
         Self {
             inner: BitVec::from_elem(m, false),
@@ -90,19 +93,24 @@ impl BloomFilter {
         }
     }
 
+    fn split_hash(hash: u128) -> (usize, usize) {
+        let h1 = (hash & 0xFFFF_FFFF_FFFF_FFFF) as usize;
+        let h2 = ((hash >> 64) & 0xFFFF_FFFF_FFFF_FFFF) as usize;
+        (h1, h2)
+    }
+
     /// Returns `true` if the item may be contained.
     ///
     /// Will never have a false negative.
     pub fn contains(&self, key: &[u8]) -> bool {
         let hash = Self::get_hash(key);
 
-        let h1 = (hash & 0xFFFF_FFFF_FFFF_FFFF) as usize;
-        let h2 = ((hash >> 64) & 0xFFFF_FFFF_FFFF_FFFF) as usize;
+        let (h1, h2) = Self::split_hash(hash);
 
-        let mut current_hash = h1;
+        let mut hash = h1;
         for _ in 0..self.k {
-            current_hash = current_hash.wrapping_add(self.k * h2);
-            let idx = current_hash % self.m;
+            hash = hash.wrapping_add(self.k * h2);
+            let idx = hash % self.m;
 
             if !self.inner.get(idx).expect("should be in bounds") {
                 return false;
@@ -114,13 +122,12 @@ impl BloomFilter {
 
     /// Adds the key to the filter
     pub fn set_with_hash(&mut self, hash: u128) {
-        let h1 = (hash & 0xFFFF_FFFF_FFFF_FFFF) as usize;
-        let h2 = ((hash >> 64) & 0xFFFF_FFFF_FFFF_FFFF) as usize;
+        let (h1, h2) = Self::split_hash(hash);
 
-        let mut current_hash = h1;
+        let mut hash = h1;
         for _ in 0..self.k {
-            current_hash = current_hash.wrapping_add(self.k * h2);
-            let idx = current_hash % self.m;
+            hash = hash.wrapping_add(self.k * h2);
+            let idx = hash % self.m;
 
             self.set_pos(idx);
         }
@@ -140,5 +147,61 @@ impl BloomFilter {
         let h2 = hasher.finish();
 
         u128::from(h1) << 64 | u128::from(h2)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use test_log::test;
+
+    #[test]
+    fn bloom_calculate_m() {
+        assert_eq!(9_592, BloomFilter::calculate_m(1_000, 0.01));
+        assert_eq!(4_800, BloomFilter::calculate_m(1_000, 0.1));
+        assert_eq!(4_792_536, BloomFilter::calculate_m(1_000_000, 0.1));
+    }
+
+    #[test]
+    fn bloom_basic() {
+        let mut filter = BloomFilter::with_fp_rate(10, 0.0001);
+
+        for key in [
+            b"item0", b"item1", b"item2", b"item3", b"item4", b"item5", b"item6", b"item7",
+            b"item8", b"item9",
+        ] {
+            assert!(!filter.contains(key));
+            filter.set_with_hash(BloomFilter::get_hash(key));
+            assert!(filter.contains(key));
+
+            assert!(!filter.contains(nanoid::nanoid!().as_bytes()));
+        }
+    }
+
+    #[test]
+    fn bloom_fpr() {
+        let item_count = 1_000_000;
+        let fpr = 0.01;
+
+        let mut filter = BloomFilter::with_fp_rate(item_count, fpr);
+
+        for key in (0..item_count).map(|_| nanoid::nanoid!()) {
+            let key = key.as_bytes();
+
+            filter.set_with_hash(BloomFilter::get_hash(key));
+            assert!(filter.contains(key));
+        }
+
+        let mut false_positives = 0;
+
+        for key in (0..item_count).map(|_| nanoid::nanoid!()) {
+            let key = key.as_bytes();
+
+            if filter.contains(key) {
+                false_positives += 1;
+            }
+        }
+
+        assert!((10_000 - false_positives) < 100);
     }
 }
