038c4362e1b9a9d7198d4d4ba26c055b401376f4
marvin-j97 <marvin.janke.97@gmail.com>
2024-05-16 21:41:02 +0200

    test: update fixture
    for one last time, believe me

--- tests/keyspace_load_fixture.rs
+++ tests/keyspace_load_fixture.rs
@@ -11,6 +11,29 @@ fn keyspace_load_v1() -> fjall::Result<()> {
     let b = keyspace.open_partition("b", Default::default())?;
     let c = keyspace.open_partition("c", Default::default())?;
 
+    /*   a.insert("a", "Only ever feeling fine")?;
+    a.insert("b", "And I'd prefer us to be close")?;
+    a.insert("c", "I'd like to look you in the eyes, not fearin'")?;
+    a.insert("d", "Starin' into the void, waitin' for replies")?;
+    a.insert("e", "Just waitin' for replies")?;
+    a.insert("f", "---")?;
+    a.insert("g", "Did you actually load this database?")?;
+    a.insert("h", "https://www.youtube.com/watch?v=eYS7xmjR4Fk")?;
+
+    a.rotate_memtable()?;
+
+    b.insert("a", "Whisper")?;
+    b.insert("b", "Answers")?;
+    b.insert("c", "There's no one")?;
+    b.insert("d", "Out there")?;
+
+    b.rotate_memtable()?;
+
+    c.insert("a", "Whisper")?;
+    c.insert("b", "Answers")?;
+    c.insert("c", "There's no one")?;
+    c.insert("d", "Out there")?; */
+
     assert_eq!(3, keyspace.partition_count());
 
     assert_eq!(1, a.tree.first_level_segment_count());
