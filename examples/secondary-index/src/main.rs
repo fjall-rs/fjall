78ad4b49e41848b08880363800fc3a7330e165ac
Marvin <33938500+marvin-j97@users.noreply.github.com>
2024-05-16 15:23:12 +0200

    Update main.rs

--- examples/secondary-index/src/main.rs
+++ examples/secondary-index/src/main.rs
@@ -1,17 +1,16 @@
-use fjall::{BlockCache, Config, Keyspace, PartitionHandle};
+use fjall::{Batch, BlockCache, Config, Keyspace, PartitionHandle};
 use format_bytes::format_bytes;
 use nanoid::nanoid;
 use std::path::Path;
 
 fn create_item(
     ks: &Keyspace,
+    batch: &Batch,
     table: &PartitionHandle,
     index: &PartitionHandle,
     name: &str,
     year: u64,
 ) -> fjall::Result<()> {
-    let mut batch = ks.batch();
-
     let id = nanoid!();
     batch.insert(table, &id, format!("{name} [{year}]"));
 
@@ -19,8 +18,6 @@ fn create_item(
     let key = format_bytes!(b"{}#{}", ts_bytes, id.as_bytes());
 
     batch.insert(index, &key, "");
-
-    batch.commit()?;
 
     Ok(())
 }
@@ -38,18 +35,22 @@ fn main() -> fjall::Result<()> {
     let items = keyspace.open_partition("items", Default::default())?;
     let sec = keyspace.open_partition("sec_idx", Default::default())?;
 
-    create_item(&keyspace, &items, &sec, "Remain in Light", 1_980)?;
-    create_item(&keyspace, &items, &sec, "Power, Corruption & Lies", 1_983)?;
-    create_item(&keyspace, &items, &sec, "Hounds of Love", 1_985)?;
-    create_item(&keyspace, &items, &sec, "Black Celebration", 1_986)?;
-    create_item(&keyspace, &items, &sec, "Disintegration", 1_989)?;
-    create_item(&keyspace, &items, &sec, "Violator", 1_990)?;
-    create_item(&keyspace, &items, &sec, "Wish", 1_991)?;
-    create_item(&keyspace, &items, &sec, "Loveless", 1_991)?;
-    create_item(&keyspace, &items, &sec, "Dummy", 1_994)?;
-    create_item(&keyspace, &items, &sec, "When The Pawn...", 1_999)?;
-    create_item(&keyspace, &items, &sec, "Kid A", 2_000)?;
-    create_item(&keyspace, &items, &sec, "Have You In My Wilderness", 2_015)?;
+    let batch = keyspace.batch();
+    create_item(&keyspace, &batch, &items, &sec, "Remain in Light", 1_980)?;
+    create_item(&keyspace, &batch, &items, &sec, "Power, Corruption & Lies", 1_983)?;
+    create_item(&keyspace, &batch, &items, &sec, "Hounds of Love", 1_985)?;
+    create_item(&keyspace, &batch, &items, &sec, "Black Celebration", 1_986)?;
+    create_item(&keyspace, &batch, &items, &sec, "Disintegration", 1_989)?;
+    create_item(&keyspace, &batch, &items, &sec, "Violator", 1_990)?;
+    create_item(&keyspace, &batch, &items, &sec, "Wish", 1_991)?;
+    create_item(&keyspace, &batch, &items, &sec, "Loveless", 1_991)?;
+    create_item(&keyspace, &batch, &items, &sec, "Dummy", 1_994)?;
+    create_item(&keyspace, &batch, &items, &sec, "When The Pawn...", 1_999)?;
+    create_item(&keyspace, &batch, &items, &sec, "Kid A", 2_000)?;
+    create_item(&keyspace, &batch, &items, &sec, "Have You In My Wilderness", 2_015)?;
+
+    batch.commit()?;
+    keyspace.persist(fjall::FlushMode::SyncAll)?;
 
     // Get items from 1990 to 2000 (exclusive)
     let lo = 1_990_u64;
@@ -66,8 +67,6 @@ fn main() -> fjall::Result<()> {
 
         eprintln!("found: {}", std::str::from_utf8(&item).unwrap());
     }
-
-    keyspace.persist(fjall::FlushMode::SyncAll)?;
 
     Ok(())
 }
