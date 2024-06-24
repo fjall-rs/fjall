674df1ee3701b6d26b3185848b63bd344a57f5ec
marvin-j97 <marvin.janke.97@gmail.com>
2024-06-24 19:04:28 +0200

    move journal flush to journal drop

--- src/keyspace.rs
+++ src/keyspace.rs
@@ -73,18 +73,9 @@ pub struct KeyspaceInner {
 
 impl Drop for KeyspaceInner {
     fn drop(&mut self) {
-        log::trace!("Dropping Keyspace, trying to flush journal");
+        log::trace!("Dropping Keyspace");
 
         self.stop_signal.send();
-
-        match self.journal.flush(PersistMode::SyncAll) {
-            Ok(()) => {
-                log::trace!("Flushed journal successfully");
-            }
-            Err(e) => {
-                log::error!("Flush error on drop: {e:?}");
-            }
-        }
 
         while self
             .active_background_threads
