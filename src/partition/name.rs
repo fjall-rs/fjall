efa4c98f895dba919056f505bfab37dc92c221d2
marvin-j97 <marvin.janke.97@gmail.com>
2024-06-30 20:45:27 +0200

    expand partition name charset

--- src/partition/name.rs
+++ src/partition/name.rs
@@ -1,7 +1,7 @@
-const VALID_CHARACTERS: &str = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_-";
+const VALID_CHARACTERS: &str = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_-.#";
 
 /// Partition names can be up to 255 characters long, can not be empty and
-/// can only contain alphanumerics, underscore (`_`) and dash (`-`).
+/// can only contain alphanumerics, underscore (`_`), dash (`-`), dot (`.`) and hash tag (`#`).
 #[allow(clippy::module_name_repetitions)]
 pub fn is_valid_partition_name(s: &str) -> bool {
     if s.is_empty() {
