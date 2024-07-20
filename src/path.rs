926b40d41b6d88d4c880ed197cb52bb4df40c884
marvin-j97 <marvin.janke.97@gmail.com>
2024-07-20 17:18:51 +0200

    refactor

--- src/path.rs
+++ src/path.rs
@@ -1,6 +1,7 @@
 use path_absolutize::Absolutize;
 use std::path::{Path, PathBuf};
 
+#[allow(clippy::module_name_repetitions)]
 pub fn absolute_path<P: AsRef<Path>>(path: P) -> PathBuf {
     // TODO: replace with https://doc.rust-lang.org/std/path/fn.absolute.html once stable
     path.as_ref()
