a5f18944edf149489aa63110bc115b93fedba6ff
Marvin <33938500+marvin-j97@users.noreply.github.com>
2024-09-23 00:27:27 +0200

    enable unwrap_used lint again

--- src/lib.rs
+++ src/lib.rs
@@ -71,7 +71,7 @@
 #![doc(html_favicon_url = "https://raw.githubusercontent.com/fjall-rs/fjall/main/logo.png")]
 #![forbid(unsafe_code)]
 #![deny(clippy::all, missing_docs, clippy::cargo)]
-// #![deny(clippy::unwrap_used)]
+#![deny(clippy::unwrap_used)]
 #![deny(clippy::indexing_slicing)]
 #![warn(clippy::pedantic, clippy::nursery)]
 #![warn(clippy::expect_used)]
