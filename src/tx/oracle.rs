82609a9a9750630eba6f1c6bbe0398993e253596
Jerome Gravel-Niquet <jeromegn@gmail.com>
2024-09-24 15:34:29 -0400

    test that the oracle garbage collects past transactions

--- src/tx/oracle.rs
+++ src/tx/oracle.rs
@@ -38,16 +38,20 @@ impl Oracle {
         // This change assumes linearizability. Lack of linearizability could
         // cause the read ts of a new txn to be lower than the commit ts of
         // a txn before it.
-        for (_ts, other_conflict_checker) in committed_txns.range((instant + 1)..) {
-            if conflict_checker.has_conflict(other_conflict_checker) {
-                return Ok(CommitOutcome::Conflicted);
-            }
-        }
+        let conflicted =
+            committed_txns
+                .range((instant + 1)..)
+                .any(|(_ts, other_conflict_checker)| {
+                    conflict_checker.has_conflict(other_conflict_checker)
+                });
 
         self.snapshot_tracker.close(instant);
         let safe_to_gc = self.snapshot_tracker.get_seqno_safe_to_gc();
-
         committed_txns.retain(|ts, _| *ts > safe_to_gc);
+
+        if conflicted {
+            return Ok(CommitOutcome::Conflicted);
+        }
 
         if let Err(e) = f() {
             return Ok(CommitOutcome::Aborted(e));
@@ -75,5 +79,49 @@ impl fmt::Display for Error {
 impl<T> From<PoisonError<T>> for Error {
     fn from(_value: PoisonError<T>) -> Self {
         Self::Poisoned
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::{Config, PartitionCreateOptions, TxKeyspace, TxPartitionHandle};
+
+    #[allow(clippy::unwrap_used)]
+    #[test]
+    fn oracle_committed_txns_does_not_leak() {
+        let tmpdir = tempfile::tempdir().unwrap();
+        let ks = Config::new(tmpdir.path()).open_transactional().unwrap();
+
+        let part = ks
+            .open_partition("foo", PartitionCreateOptions::default())
+            .unwrap();
+
+        for _ in 0..250 {
+            run_tx(&ks, &part).unwrap();
+        }
+
+        assert!(dbg!(ks.orc.write_serialize_lock.lock().unwrap().len()) < 200);
+
+        for _ in 0..200 {
+            run_tx(&ks, &part).unwrap();
+        }
+
+        assert!(dbg!(ks.orc.write_serialize_lock.lock().unwrap().len()) < 200);
+    }
+
+    fn run_tx(ks: &TxKeyspace, part: &TxPartitionHandle) -> Result<(), Box<dyn std::error::Error>> {
+        let mut tx1 = ks.write_tx()?;
+        let mut tx2 = ks.write_tx()?;
+        tx1.insert(part, "hello", "world");
+
+        tx1.commit()??;
+        assert!(part.contains_key("hello")?);
+
+        _ = tx2.get(part, "hello")?;
+
+        tx2.insert(part, "hello", "world2");
+        assert!(tx2.commit()?.is_err()); // intended to conflict
+
+        Ok(())
     }
 }
