9abb5955226cc22b90e6c9b3577249c75816aeb0
marvin-j97 <marvin.janke.97@gmail.com>
2024-10-18 23:05:20 +0200

    don't insert in FAU/UAF if value hasn't changed

--- src/tx/write/mod.rs
+++ src/tx/write/mod.rs
@@ -87,7 +87,10 @@ impl BaseTransaction {
         let updated = f(prev.as_ref());
 
         if let Some(value) = &updated {
-            self.insert(partition, &key, value);
+            // NOTE: Skip insert if the value hasn't changed
+            if updated != prev {
+                self.insert(partition, &key, value);
+            }
         } else if prev.is_some() {
             self.remove(partition, &key);
         }
@@ -114,8 +117,11 @@ impl BaseTransaction {
         let prev = self.get(partition, &key)?;
         let updated = f(prev.as_ref());
 
-        if let Some(value) = updated {
-            self.insert(partition, &key, value);
+        if let Some(value) = &updated {
+            // NOTE: Skip insert if the value hasn't changed
+            if updated != prev {
+                self.insert(partition, &key, value);
+            }
         } else if prev.is_some() {
             self.remove(partition, &key);
         }
