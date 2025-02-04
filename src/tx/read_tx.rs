44249959c8888be076ca4899f290f1785a08498e
Marvin <33938500+marvin-j97@users.noreply.github.com>
2025-02-04 17:32:15 +0100

    Update read_tx.rs

--- src/tx/read_tx.rs
+++ src/tx/read_tx.rs
@@ -367,7 +367,7 @@ impl ReadTransaction {
         crate::iter::Iter::new(self.nonce.clone(), iter)
     }
 
-    /// Iterates over a prefixed range of the transaction's state.
+    /// Iterates over a prefixed set of the transaction's state.
     ///
     /// Avoid using an empty prefix as it may scan a lot of items (unless limited).
     ///
