faa64053a0195fd67efcf8b3f295f2a9e4834b7b
Marvin <33938500+marvin-j97@users.noreply.github.com>
2025-07-21 16:09:57 +0200

    wip

--- tests/recovery_journal_mac.rs
+++ tests/recovery_journal_mac.rs
@@ -2,7 +2,7 @@ use fjall::Config;
 use test_log::test;
 
 #[test]
-fn keyspace_journal_recovery_mac_underscore_file() -> fjall::Result<()> {
+fn keyspace_journal_recovery_mac_ds_store() -> fjall::Result<()> {
     let folder = tempfile::tempdir()?;
 
     {
