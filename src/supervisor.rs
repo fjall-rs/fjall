d21be4091d8b43cab67078313a901ace3fde3f5b
marvin-j97 <marvin.janke.97@gmail.com>
2025-11-09 15:50:12 +0100

    license header

--- src/supervisor.rs
+++ src/supervisor.rs
@@ -1,3 +1,7 @@
+// Copyright (c) 2024-present, fjall-rs
+// This source code is licensed under both the Apache 2.0 and MIT License
+// (found in the LICENSE-* files in the repository)
+
 use crate::{
     compaction::manager::CompactionManager, flush::new_manager::FlushNewManager,
     journal::manager::JournalManager, snapshot_tracker::SnapshotTracker,
